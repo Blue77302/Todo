fc9520a7a6713f075f96b49887cfe0227876efa4
Blue77302 <letanduong0304@gmail.com>
2024-01-20 12:26:40 +0700

    init project

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trang Home</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        body {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            min-height: 100vh;
+            background-color: #fff; 
+        }
+
+        h2 {
+            color: #ffcc00
+        }
+    </style>
+</head>
+<body>
+    <h2 class="text-center">Lê Tấn Dương</h2>
+</body>
+</html>
